32ec4d4ed5b6241565f6adc28d5ffacab49ced85
ffmcgee <jc.992@hotmail.com>
2022-05-15 16:23:32 +0000

    add bulma and styled components. design basic interface to interact with smart contracts.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 function App() {
+  useEffect(() => {
+    const loadProvider = async () => {
+      // with metamask we have an access to window.ethereum & window.web3
+      // metamask injects a global API into app
+      // this API allows app to request users, accounts, read data to blockchain
+      // sign messages and trxs
+      console.log(window.web3);
+      console.log(window.ethereum);
+    };
+
+    loadProvider();
+  }, []);
+
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <>
+      <Wrapper>
+        <div className="faucet">
+          <div className="balance-view is-size-2">
+            Current Balance: <strong>10</strong> ETH
+          </div>
+          <button
+            onClick={async () => {
+              const accounts = await window.ethereum.request({
+                method: 'eth_requestAccounts',
+              });
+              console.log({ accounts });
+            }}
+            className="btn mr-2"
+          >
+            Enable Ethereum
+          </button>
+          <button className="btn mr-2">Donate</button>
+          <button className="btn">Withdraw</button>
+        </div>
+      </Wrapper>
+    </>
   );
 }
 
