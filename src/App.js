0554229db4e98a1c7416bc9798aa62f48b72d9d2
ffmcgee <jc.992@hotmail.com>
2022-05-15 16:36:06 +0000

    add web3 dependency. load provider & request account access when component is mounted

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import Web3 from 'web3';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -9,6 +10,11 @@ const Wrapper = styled.div`
 `;
 
 function App() {
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null,
+  });
+
   useEffect(() => {
     const loadProvider = async () => {
       // with metamask we have an access to window.ethereum & window.web3
@@ -17,10 +23,32 @@ function App() {
       // sign messages and trxs
       console.log(window.web3);
       console.log(window.ethereum);
+
+      let provider = null;
+
+      if (window.ethereum) {
+        provider = window.ethereum;
+        try {
+          await provider.enable();
+        } catch {
+          console.error('User denied account access!');
+        }
+      } else if (window.web3) {
+        provider = window.web3.currentProvider;
+      } else if (!process.env.production) {
+        provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      }
+
+      setWeb3Api({
+        web3: new Web3(provider),
+        provider,
+      });
     };
 
     loadProvider();
   }, []);
+
+  console.log({ web3Api: web3Api.web3 });
 
   return (
     <>
@@ -29,7 +57,8 @@ function App() {
           <div className="balance-view is-size-2">
             Current Balance: <strong>10</strong> ETH
           </div>
-          <button
+          {/* Button to manually request access to account */}
+          {/* <button
             onClick={async () => {
               const accounts = await window.ethereum.request({
                 method: 'eth_requestAccounts',
@@ -39,7 +68,7 @@ function App() {
             className="btn mr-2"
           >
             Enable Ethereum
-          </button>
+          </button> */}
           <button className="btn mr-2">Donate</button>
           <button className="btn">Withdraw</button>
         </div>
