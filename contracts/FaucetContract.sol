941029c4bf9781071ca669177c49af04f8ce91b7
ffmcgee <jc.992@hotmail.com>
2022-05-15 15:51:10 +0000

    update solidity compiler and FaucetContract.

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,8 +2,9 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-    //storage variables
-    uint public funds = 1000; //positive values only
-    int public counter = -10;
-    uint32 public test = 3834674236;
+	// Function called when you make a tx that doesn't specify function name to call.
+	
+	// External functions are part of the contract interface, which means they can be called from other contracts and/or txs.
+
+  receive() external payable {}
 }
